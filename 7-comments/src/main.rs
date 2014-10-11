854ba9215454ac04bbac3309114af6cea0223866
Riley Teige <riley.teige@gmail.com>
2014-10-11 09:33:04 -0500

    Adding 7-comments.

---
+++ 7-comments/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    // Rust has line comments like many other languages. 
+    hello("Riley");
+}
+
+/// `hello` is a function that prints a greeting that is personalized based on
+/// the name given.
+///
+/// # Arguments
+///
+/// * `name` - The name of the person you'd like to greet.
+///
+/// # Example
+///
+/// ```rust
+/// let name = "Steve";
+/// hello(name); // prints "Hello, Steve!"
+/// ```
+
+fn hello(name: &str) {
+	println!("Hello, {}!", name);
+}
