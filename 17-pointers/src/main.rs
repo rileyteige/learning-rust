b4fce3b2eb1d3cf6426dc23b03567f1620a97e34
Riley Teige <riley.teige@gmail.com>
2014-10-15 20:55:01 -0500

    Adding 17-pointers.

---
+++ 17-pointers/src/main.rs
@@ -0,0 +1,33 @@
+use std::rc::Rc;
+
+fn main() {
+    // Simple reference
+    let x = 5i;
+    let y = &x;
+
+    assert_eq!(5i, *y);
+
+    assert_eq!(6, add_one(y));
+
+    // Multiple aliased references
+    let z = &x;
+
+    let mut x2 = 5i;
+
+    // Allocate a variable on the heap. Will automatically be freed.
+    let mut x3 = box 5i;
+
+	assert_eq!(5i, *x3);
+
+	rc_and_arc();
+}
+
+fn add_one(x: &int) -> int { *x + 1 }
+
+fn rc_and_arc() {
+	let x = Rc::new(5i);
+
+	// How to use Arc<T>?
+
+	assert_eq!(5i, *x);
+}
